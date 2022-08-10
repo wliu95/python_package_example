0d2717840eea0f5c1759c7fc4c91c9529ef4e070
catna <catnaer@163.com>
2022-08-10 21:35:57 +0800

    initial commit

---
+++ jupyter/print_functions.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+
+
+def test_print_logic():
+    pprint({
+        "key": "value",
+        "music": {
+            "eason Chan": "渐渐",
+            "eason Chan": "US",
+            "jay Chou": "你听得到"
+        }
+    })
+
+
+if __name__ == "__main__":
+    test_print_logic()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="python package example",
+    version="0.0.1",
+    description="test package usage",
+    packages=[
+        "jupyter"
+    ]
+)
